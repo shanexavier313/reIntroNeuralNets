04ded14c92ba91f602c0e66d03ff2cb48aab0408
ericbrown <ericbrown1.2018@u.northwestern.edu>
2019-03-16 21:15:38 -0400

    updated with python3 code

--- HiddenLayer.py
+++ HiddenLayer.py
@@ -2,40 +2,46 @@ from sklearn import datasets
 import numpy as np
 import matplotlib.pyplot as plt
 
-np.random.seed(99)
+np.random.seed(0)
 feature_set, labels = datasets.make_moons(100, noise=0.10)
 plt.figure(figsize=(10, 7))
-plt.scatter(feature_set[:, 0], feature_set[:, 1], c=labels, cmap=plt.cm.witner)
+plt.scatter(feature_set[:, 0], feature_set[:, 1], c=labels, cmap=plt.cm.winter)
 
 labels = labels.reshape(100, 1)
+
 
 def sigmoid(x):
     return 1/(1+np.exp(-x))
 
+
 def sigmoid_der(x):
     return sigmoid(x) * (1-sigmoid(x))
 
-wh = np.random.rand(len(feature_set[0]),4)
-wo = np.random.rand(4,1)
+
+wh = np.random.rand(len(feature_set[0]), 4)
+wo = np.random.rand(4, 1)
 lr = 0.5
+
 
 for epoch in range(200000):
     # feedforward
     zh = np.dot(feature_set, wh)
     ah = sigmoid(zh)
 
-    zo = np.dot(ah,wo)
+    zo = np.dot(ah, wo)
     ao = sigmoid(zo)
 
+
+
     # Phase1 ==========================
-    error_out = ((1/3) * (np.power((ao - labels),2)))
+    error_out = ((1/2) * (np.power((ao - labels), 2)))
     print(error_out.sum())
 
     dcost_dao = ao - labels
     dao_dzo = sigmoid_der(zo)
     dzo_dwo = ah
 
-    dcost_wo = np.do(dzo_dwo.T, dcost_dao * dao_dzo)
+    dcost_wo = np.dot(dzo_dwo.T, dcost_dao * dao_dzo)
 
     # Phase 2 ===========================
     dcost_dzo = dcost_dao * dao_dzo
@@ -48,5 +54,5 @@ for epoch in range(200000):
     # Update Weights ====================
 
     wh -= lr * dcost_wh
-    wo -= lr *dcost_wh
+    wo -= lr * dcost_wo
 

b0cee59142348499a96922e603002f529d7fa2d6
ericbrown <ericbrown1.2018@u.northwestern.edu>
2019-03-17 12:07:36 -0400

    Tuned Learning Rate

--- HiddenLayer.py
+++ HiddenLayer.py
@@ -18,10 +18,37 @@ def sigmoid_der(x):
     return sigmoid(x) * (1-sigmoid(x))
 
 
+np.random.seed(32)
 wh = np.random.rand(len(feature_set[0]), 4)
 wo = np.random.rand(4, 1)
-lr = 0.5
+error_out = 0
 
+## optimizing learning rate
+# bases = np.repeat(10, 3)
+# exponents_1 = -(np.random.rand(3) + 3)
+# exponents_2 = -(np.random.rand(3) + 2)
+
+# first test of learning rate
+# lr = [1, 0.5, 0.1, 0.01, 0.001, 0.0001]
+
+# second test of learning rate
+# lr = np.power(bases, exponents_1).tolist() + np.power(bases, exponents_2).tolist()
+
+# third test of learning rate
+# lr = [.005, .006, .007, .008, .009, .01]
+
+# fourth test of learning rate
+# lr = [.01, .03, .05, .07, .09, .11]
+
+# best learning rate:
+lr = .05
+results = []
+
+print(lr)
+
+
+# for x in range(0, 6):
+#     lr_x = lr[x]
 
 for epoch in range(200000):
     # feedforward
@@ -31,11 +58,8 @@ for epoch in range(200000):
     zo = np.dot(ah, wo)
     ao = sigmoid(zo)
 
-
-
     # Phase1 ==========================
     error_out = ((1/2) * (np.power((ao - labels), 2)))
-    print(error_out.sum())
 
     dcost_dao = ao - labels
     dao_dzo = sigmoid_der(zo)
@@ -52,7 +76,16 @@ for epoch in range(200000):
     dcost_wh = np.dot(dzh_dwh.T, dah_dzh * dcost_dah)
 
     # Update Weights ====================
-
     wh -= lr * dcost_wh
     wo -= lr * dcost_wo
 
+print(error_out.sum())
+
+#print(results[x])
+
+# Plots to identify the best learning rate
+# plt.plot(lr, results)
+# plt.scatter(lr, results)
+# plt.xlabel("Learning Rate")
+# plt.ylabel("Mean Squared Error")
+# plt.show()
--- README.md
+++ README.md
@@ -9,4 +9,7 @@ Finally we evolved the network to enable multi-class Classification
 
 This project was modeled on the articles by [Usman Malik](https://stackabuse.com/creating-a-neural-network-from-scratch-in-python-multi-class-classification/)
 
- 
+## Optimization 
+When testing the binary classification neural network, it quickly became obvious that the results varied significantly depending on the random seed. A random seed of 0 resulted in a relatively low ~1.5 MSE. But many other seeds (50,75,100) where significantly higher between ~4-5 MSE. 
+
+It was clear that we needed to optimize the model. To maintain simplicity, we focused on tuning the learning rate. In future exercises we will work on tuning the number of hidden layers and the number of units in each of those layers.  
